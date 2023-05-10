cdd25215afda1761ae627ce08f90d36567fdb551
rayr482 <rar002@ucsd.edu>
2023-05-10 15:58:57 -0700

    added basic html without classes and a couple variables in js

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var apiKey = '3ab465212cd66bf0d2eb61e9e8abcb78';
+var cityInput;
+var cityLat;
+var cityLon;
+var apiURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ cityLat + '&lon=' + cityLon + '&appid=' + apiKey;
+
+var getCity = function() {
+    
+}
